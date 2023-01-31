19cf02bfeafce979ba3f4b9748043d1bd14ec38c
Carlmarion <marion.carl498@gmail.com>
2023-01-31 10:45:00 +0000

    solved Docker issue and added ApolloServer working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("here i am to save the day");
+import createServer from "./tools/server";
+
+const port = 5002;
+
+const start = async (): Promise<void> => {
+  const server = await createServer();
+  try {
+    const { url }: { url: string } = await server.listen({ port });
+    console.log(`server ready at port ${url}`);
+  } catch (e) {
+    console.error("Error starting the server");
+  }
+};
+
+void start();
