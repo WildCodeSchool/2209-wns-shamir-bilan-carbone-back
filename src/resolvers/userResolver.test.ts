5830dcee0d5dccb67d4a5a408ec40ed753148988
Agnija <ilzena.agnija@gmail.com>
2023-03-03 12:01:10 +0100

    resolv confl#2

--- src/resolvers/userResolver.test.ts
+++ src/resolvers/userResolver.test.ts
@@ -8,25 +8,6 @@ describe("User resolver", () => {
     server = await createServer();
   });
 
-  // it("should retrieve a token", async () => {
-  //   const getTokenMutation = gql`
-  //     mutation GetToken($password: String!, $email: String!) {
-  //       getToken(password: $password, email: $email)
-  //     }
-  //   `;
-
-  const response = await server.executeOperation({
-    query: getTokenMutation,
-    variables: {
-      password: "12345",
-      email: "anna@gmail.com",
-    },
-  });
-
-  //   expect(response.errors).toBeUndefined();
-  //   expect(response.data?.getToken).toBeDefined();
-  // });
-
   it("should not retrieve a token", async () => {
     const getTokenMutation = gql`
       mutation GetToken($password: String!, $email: String!) {
